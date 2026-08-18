589db80aee102773699e42584646addb665b16be
John <abanjohnpatrick@gmail.com>
2026-08-18 18:34:37 +0800

    Patient-service: Service and Repository

---
+++ patient-service/src/main/java/com/noir/patientservice/service/PatientService.java
@@ -0,0 +1,20 @@
+package com.noir.patientservice.service;
+
+import com.noir.patientservice.model.Patient;
+import com.noir.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+}
