2a9dc885bab6e274bb9d4ad21c56b8f6d7c3cb49
John <abanjohnpatrick@gmail.com>
2026-08-29 19:38:50 +0800

    Billing-service: grpc server request

--- billing-service/src/main/java/com/noir/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/noir/billingservice/grpc/BillingGrpcService.java
@@ -13,7 +13,8 @@ public class BillingGrpcService extends BillingServiceImplBase {
     private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
 
     @Override
-    public void createBillingAccount(BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver) {
+    public void createBillingAccount(BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver) {
 
         log.info("createBillingAccount request received {}", billingRequest.toString());
 
